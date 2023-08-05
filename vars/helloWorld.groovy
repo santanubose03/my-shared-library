2b2a72897675fd96c7ec876b0c98f951927041ab
santanubose03 <santanubose03@yahoo.com>
2023-08-05 13:23:04 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,3 @@
-def call(String name, String dayOfWeek){
-  sh "echo Hello $name, Today is $dayOfWeek"
+def call(Map stringParams){
+  sh "echo Hello ${stringParams.name}, Today is ${stringParams.dayOfWeek}"
 }
