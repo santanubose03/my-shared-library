57e4023bacd5e5ed5d9085124d03b41c70c151a3
santanubose03 <santanubose03@yahoo.com>
2023-08-05 23:10:43 +0530

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,4 +1,5 @@
 def call(Map scripts=[:]){
+    sh "echo entered loadlinuxscript file"
     def scriptContents = libraryResource "${scripts.filename}"
     writeFile file:"${scripts.filename}", text:scriptContents
     sh "chmod a+x ./${scripts.filename}"
