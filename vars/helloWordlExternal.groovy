b325bcb0d2b0576d305630a26a9d0c43496371d8
santanubose03 <santanubose03@yahoo.com>
2023-08-05 23:23:34 +0530

    Update helloWordlExternal.groovy

--- vars/helloWordlExternal.groovy
+++ vars/helloWordlExternal.groovy
@@ -1,4 +1,5 @@
 def call(Map config=[:]){
+    sh "echo entered helloWorldExternal file"
     loadLinuxScript(filename: 'hello-world.sh')
     sh "./hello-world.sh ${config.name} ${config.dayOfWeek}"
 }
