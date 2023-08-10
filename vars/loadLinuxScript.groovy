02d52eb17ae9e40dce45141bed7d35483936a6fc
santanubose03 <santanubose03@yahoo.com>
2023-08-10 22:18:49 +0530

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,6 @@
 def call(Map config=[:]){
     def scriptContents = libraryResource "${config.filename}"
     writeFile file:"${config.filename}", text:scriptContents
+    sh "dos2unix ${WORKSPACE}/hello-world.sh"
     sh "chmod +x ./${config.filename}"
 }
